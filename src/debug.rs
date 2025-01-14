819844d66a8697fd1dc0e438da7ca2b1d6a23edc
Angelo Anthony Rettob <7332321+antjowie@users.noreply.github.com>
2025-01-14 15:42:18 +0100

    Downgrade egui to support world inspector plugin

--- src/debug.rs
+++ src/debug.rs
@@ -1,13 +1,61 @@
 use bevy::input::common_conditions::input_toggle_active;
 use bevy::prelude::*;
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy::window::PrimaryWindow;
+use bevy_egui::{egui, EguiContext};
+use bevy_inspector_egui::bevy_inspector::hierarchy::SelectedEntities;
+use bevy_inspector_egui::DefaultInspectorConfigPlugin;
 
 pub struct DebugPlugin;
 
 impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(
-            WorldInspectorPlugin::new().run_if(input_toggle_active(false, KeyCode::Delete)),
+        app.add_plugins(DefaultInspectorConfigPlugin);
+        app.add_systems(
+            Update,
+            inspector_ui.run_if(input_toggle_active(false, KeyCode::Delete)),
         );
     }
+}
+
+fn inspector_ui(world: &mut World, mut selected_entities: Local<SelectedEntities>) {
+    let mut egui_context = world
+        .query_filtered::<&mut EguiContext, With<PrimaryWindow>>()
+        .single(world)
+        .clone();
+    egui::SidePanel::right("hierarchy")
+        .default_width(200.0)
+        .show(egui_context.get_mut(), |ui| {
+            egui::ScrollArea::both().show(ui, |ui| {
+                ui.heading("Hierarchy");
+
+                bevy_inspector_egui::bevy_inspector::hierarchy::hierarchy_ui(
+                    world,
+                    ui,
+                    &mut selected_entities,
+                );
+
+                ui.allocate_space(ui.available_size());
+            });
+        });
+
+    egui::SidePanel::right("inspector")
+        .default_width(250.0)
+        .show(egui_context.get_mut(), |ui| {
+            egui::ScrollArea::both().show(ui, |ui| {
+                ui.heading("Inspector");
+
+                match selected_entities.as_slice() {
+                    &[entity] => {
+                        bevy_inspector_egui::bevy_inspector::ui_for_entity(world, entity, ui);
+                    }
+                    entities => {
+                        bevy_inspector_egui::bevy_inspector::ui_for_entities_shared_components(
+                            world, entities, ui,
+                        );
+                    }
+                }
+
+                ui.allocate_space(ui.available_size());
+            });
+        });
 }
